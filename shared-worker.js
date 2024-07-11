922e2436c4f2ee1943412ed6de930e4f8f61bc89
vthorat-csod <vthorat@csod.com>
2024-07-11 12:11:33 +0530

    Updates

---
+++ shared-worker.js
@@ -0,0 +1,22 @@
+// shared-worker.js
+const connections = [];
+
+onconnect = function(e) {
+    const port = e.ports[0];
+    connections.push(port);
+
+    console.log('Shared Worker connected.');
+
+    port.onmessage = function(event) {
+        console.log('Message received in Shared Worker:', event.data);
+
+        // Broadcast the message to all connected ports
+        connections.forEach(conn => {
+            if (conn !== port) {
+                conn.postMessage(event.data);
+            }
+        });
+    };
+
+    port.start();
+};
